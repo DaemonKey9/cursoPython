291c2d1b170a53ae5366d48443fc297f18fd8d30
MOZAR <poma04546@gmail.com>
2022-11-15 13:17:05 -0500

    clase_15/11/22

---
+++ tarea/estructuradatos/main.py
@@ -0,0 +1,14 @@
+##estructura de datos
+##listas
+##metodos para estructura de dato tipo lista  en python
+
+# list=['deyanira']#lista vacia
+# list.append('diego')
+# list.insert(0,'edith')
+# list.insert(len(list),'jamely')
+# print(list)
+
+list=[]
+for i in range(11):
+    list.append(1)
+print(list)
